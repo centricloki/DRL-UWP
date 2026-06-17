cca9fb55e6a162467c0e5e9babc4cc346948d98a
Lokender Tiwari <lokender.tiwari@centricconsulting.com>
2026-06-17 14:41:54 +0530

    Terra Map 1.0.45

--- DRLMobile.Uwp/View/ViewRouteListPage.xaml.cs
+++ DRLMobile.Uwp/View/ViewRouteListPage.xaml.cs
@@ -3,12 +3,14 @@ using DRLMobile.Uwp.CustomControls;
 using DRLMobile.Uwp.Helpers;
 using DRLMobile.Uwp.ViewModel;
 using DRLMobile.ExceptionHandler;
+
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
@@ -251,55 +253,96 @@ namespace DRLMobile.Uwp.View
                 }
 
                 if (ViewModel != null && ViewModel.PointOfInterestSource != null && ViewModel.PointOfInterestSource.Count > 0)
-                {                  
+                {
                     var startItem = ViewModel.PointOfInterestSource.FirstOrDefault();
                     var endItem = ViewModel.PointOfInterestSource.LastOrDefault();
 
-                    //string startImgUri = startItem != null ? startItem.ImageSourceUri : null;
-                    //string endImgUri = endItem != null ? endItem.ImageSourceUri : null;
-                    //const string intermediateUri = "ms-appx:///Assets/Maps/MapPin-Red.png";
-
-                    //if (!string.IsNullOrEmpty(startImgUri) && !string.IsNullOrEmpty(endImgUri))
-                    //{
-                        //var startIconTask = GetCachedIconAsync(startImgUri, token);
-                        //var endIconTask = GetCachedIconAsync(endImgUri, token);
-                        //var intermediateIconTask = GetCachedIconAsync(intermediateUri, token);
-
-                        //await Task.WhenAll(startIconTask, endIconTask, intermediateIconTask);
-
-                        var line = new OnTerra.MapsControl.UWP.MapPolyline
-                        {
-                            StrokeColor = Colors.Blue,
-                            StrokeThickness = 4,
-                            StrokeDashed = false,
-                            StartMarkerLabel = "Start",
-                            EndMarkerLabel = "End",
-                            //StartMarkerImage = await startIconTask,
-                            //EndMarkerImage = await endIconTask,
-                            //IntermediateMarkerImage = await intermediateIconTask,
-                            Path = new OnTerra.MapsControl.UWP.Geopath(
-                                ViewModel.PointOfInterestSource.Select(x =>
-                                 new OnTerra.MapsControl.UWP.BasicGeoposition
-                                 {
-                                     Latitude = x.OnTerraLocation.Position.Latitude,
-                                     Longitude = x.OnTerraLocation.Position.Longitude,
-                                     Tag = x?.CustomerData?.CustomerID.ToString()
-                                 }))
-                        };
-
-                        if (myMap != null)
-                        {
-                            await myMap.PolylineAsync(line);
-                            OnTerra.MapsControl.UWP.GeoboundingBox geoboundingBox = OnTerra.MapsControl.UWP.GeoboundingBox.TryCompute(
-                                ViewModel.PointOfInterestSource.Select(x =>
+                    var points = ViewModel.PointOfInterestSource.Select(x =>
                              new OnTerra.MapsControl.UWP.BasicGeoposition
                              {
                                  Latitude = x.OnTerraLocation.Position.Latitude,
                                  Longitude = x.OnTerraLocation.Position.Longitude,
-                             }));
-                            await myMap.TrySetViewBoundsAsync(geoboundingBox, null, OnTerra.MapsControl.UWP.MapAnimationKind.Default);
-                        }
-                    //}
+                             });
+
+                    var stops = new List<OnTerra.MapsControl.UWP.PolylineStopInfo>();
+                    for (int i = 0; i < ViewModel.PointOfInterestSource.Count; i++)
+                    {
+                        stops.Add(new OnTerra.MapsControl.UWP.PolylineStopInfo
+                        {
+                            Index = i,
+                            Title = ViewModel.PointOfInterestSource[i]?.RouteData?.CustomerNumber
+                        });
+                    }
+
+            //        List<OnTerra.MapsControl.UWP.BasicGeoposition> points = new List<OnTerra.MapsControl.UWP.BasicGeoposition>
+            //{
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 27.9506, Longitude = -82.4572 }, // 1: Tampa, FL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 28.5383, Longitude = -81.3792 }, // 2: Orlando, FL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 26.7153, Longitude = -80.0534 }, // 3: West Palm Beach, FL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 25.7617, Longitude = -80.1918 }, // 4: Miami, FL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 29.2108, Longitude = -81.0228 }, // 5: Daytona Beach, FL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 30.3322, Longitude = -81.6557 }, // 6: Jacksonville, FL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 32.0809, Longitude = -81.0912 }, // 7: Savannah, GA
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 32.7765, Longitude = -79.9311 }, // 8: Charleston, SC
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 33.6891, Longitude = -78.8867 }, // 9: Myrtle Beach, SC
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 35.7796, Longitude = -78.6382 }, // 10: Raleigh, NC
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 35.2271, Longitude = -80.8431 }, // 11: Charlotte, NC
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 34.8526, Longitude = -82.3940 }, // 12: Greenville, SC
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 36.1627, Longitude = -86.7816 }, // 13: Nashville, TN
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 34.7304, Longitude = -86.5861 }, // 14: Huntsville, AL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 33.5186, Longitude = -86.8104 }, // 15: Birmingham, AL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 32.3668, Longitude = -86.3000 }, // 16: Montgomery, AL
+            //    new OnTerra.MapsControl.UWP.BasicGeoposition { Latitude = 30.6954, Longitude = -88.0399 }  // 17: Mobile, AL
+            //};
+            //        var stops = new List<OnTerra.MapsControl.UWP.PolylineStopInfo>();
+            //        for (int i = 0; i < points.Count; i++)
+            //        {
+            //            stops.Add(new OnTerra.MapsControl.UWP.PolylineStopInfo
+            //            {
+            //                Index = i,
+            //                Title = $"C{(120001 + i).ToString()}"
+            //            });
+            //        }
+
+
+
+                    var line = new OnTerra.MapsControl.UWP.MapPolyline
+                    {
+                        StrokeColor = Colors.Blue,
+                        StrokeThickness = 4,
+                        StrokeDashed = false,
+                        StartMarkerLabel = "Start",
+                        EndMarkerLabel = "End",
+                        ShowStopMarkers = true,
+                        //Path = new OnTerra.MapsControl.UWP.Geopath(
+                        //        ViewModel.PointOfInterestSource.Select(x =>
+                        //         new OnTerra.MapsControl.UWP.BasicGeoposition
+                        //         {
+                        //             Latitude = x.OnTerraLocation.Position.Latitude,
+                        //             Longitude = x.OnTerraLocation.Position.Longitude,
+                        //             Tag = x?.CustomerData?.CustomerID.ToString()
+                        //         })),
+                        Path = new OnTerra.MapsControl.UWP.Geopath(points),
+                        Stops = stops
+                    };
+
+                    if (myMap != null)
+                    {
+                        //await myMap.PolylineAsync(line);
+                        //OnTerra.MapsControl.UWP.GeoboundingBox geoboundingBox = OnTerra.MapsControl.UWP.GeoboundingBox.TryCompute(
+                        //    ViewModel.PointOfInterestSource.Select(x =>
+                        // new OnTerra.MapsControl.UWP.BasicGeoposition
+                        // {
+                        //     Latitude = x.OnTerraLocation.Position.Latitude,
+                        //     Longitude = x.OnTerraLocation.Position.Longitude,
+                        // }));
+                        //await myMap.TrySetViewBoundsAsync(geoboundingBox, null, OnTerra.MapsControl.UWP.MapAnimationKind.Default);
+
+                        await myMap.PolylineAsync(line);
+
+                        OnTerra.MapsControl.UWP.GeoboundingBox geoboundingBox = OnTerra.MapsControl.UWP.GeoboundingBox.TryCompute(points);
+                        await myMap.TrySetViewBoundsAsync(geoboundingBox, new Thickness(50), OnTerra.MapsControl.UWP.MapAnimationKind.Default);
+                    }
                 }
             }
             catch (OperationCanceledException)
@@ -335,14 +378,13 @@ namespace DRLMobile.Uwp.View
 
                 if (mapClickedIcon != null)
                 {
-                    // var currentPoint = mapClickedIcon.Tag as PointOfInterest;
-                    if (int.TryParse(mapClickedIcon.Title, out int _customerId))
+                    if (!string.IsNullOrWhiteSpace(mapClickedIcon.Title))
                     {
-                        var currentPoint = ViewModel.PointOfInterestSource.Where(x => x.CustomerData != null).FirstOrDefault(x => x?.CustomerData.CustomerID == _customerId);
-                        if (currentPoint != null && currentPoint.CustomerData != null)
+                        var currentPoint = ViewModel.PointOfInterestSource.Where(x => x.RouteData != null).FirstOrDefault(x => x?.RouteData.CustomerNumber == mapClickedIcon.Title);
+                        if (currentPoint != null && currentPoint.RouteData != null)
                         {
-                            customMapPinPopup.DeviceCustomerId = currentPoint.CustomerData.DeviceCustomerID;
-                            customMapPinPopup.CustomerId = currentPoint.CustomerData.CustomerID;
+                            customMapPinPopup.DeviceCustomerId = currentPoint.RouteData.DeviceCustomerID;
+                            customMapPinPopup.CustomerId = currentPoint.RouteData.CustomerID;
 
                             if (ViewModel != null)
                             {
